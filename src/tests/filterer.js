8aea15fd54e8135f8d895f87319fa1d0ad839450
Jacob Sowles <jacob.sowles@speakcreative.com>
2016-10-21 17:36:21 -0700

    feat: add completion and status filtering to utility class

--- src/tests/filterer.js
+++ src/tests/filterer.js
@@ -2,27 +2,81 @@ import Filterer from '@/utils/filterer';
 import { completion, date, status } from '@/utils/filter-values';
 
 (function() {
-    describe('filterByTaskGroup', () => {
-        it('should only return tasks in specified task group', () => {
-            givenFilters({ taskGroupId: 1 });
-            whenFiltersApplied();
+    describe('filterByCompletion', () => {
+        it('should only return complete tasks when complete filter specified', () => {
+            givenFilters({ completion: completion.COMPLETE });
+            whenCompletionFilterApplied();
+            thenResultCountShouldBe(1);
+            thenResultIdsShouldBe([1]);
+        });
+
+        it('should only return incomplete tasks when incomplete filter specified', () => {
+            givenFilters({ completion: completion.INCOMPLETE });
+            whenCompletionFilterApplied();
+            thenResultCountShouldBe(2);
+            thenResultIdsShouldBe([2,3]);
+        });
+
+        it('should return all tasks when no filter specified', () => {
+            givenFilters({});
+            whenCompletionFilterApplied();
+            thenResultCountShouldBe(3);
+            thenResultIdsShouldBe([1,2,3]);
+        });
+    });
+
+    describe('filterByStatus', () => {
+        it('should only return blocked tasks when only blocked filter specified', () => {
+            givenFilters({ status: [status.BLOCKED] });
+            whenStatusFilterApplied();
+            thenResultCountShouldBe(1);
+            thenResultIdsShouldBe([1]);
+        });
+
+        it('should only return delegated tasks when only delegated filter specified', () => {
+            givenFilters({ status: [status.DELEGATED] });
+            whenStatusFilterApplied();
             thenResultCountShouldBe(1);
             thenResultIdsShouldBe([2]);
         });
 
-        it('should return all tasks when no task group specified', () => {
-            givenFilters({});
-            whenFiltersApplied();
+        it('should only return blocked and delegated tasks when both filters specified', () => {
+            givenFilters({ status: [status.BLOCKED, status.DELEGATED] });
+            whenStatusFilterApplied();
             thenResultCountShouldBe(2);
             thenResultIdsShouldBe([1,2]);
+        });
+
+        it('should return all tasks when no filters specified', () => {
+            givenFilters({});
+            whenStatusFilterApplied();
+            thenResultCountShouldBe(3);
+            thenResultIdsShouldBe([1,2,3]);
+        });
+    });
+
+    describe('filterByTaskGroup', () => {
+        it('should only return tasks in specified task group', () => {
+            givenFilters({ taskGroupId: 1 });
+            whenTaskGroupFilterApplied();
+            thenResultCountShouldBe(2);
+            thenResultIdsShouldBe([2,3]);
+        });
+
+        it('should return all tasks when no task group specified', () => {
+            givenFilters({});
+            whenTaskGroupFilterApplied();
+            thenResultCountShouldBe(3);
+            thenResultIdsShouldBe([1,2,3]);
         });
     });
 
     //// PRIVATE MEMBERS ////
 
     const _tasks = [
-        { Id: 1, TaskGroupId: null },
-        { Id: 2, TaskGroupId: 1 }
+        { Id: 1, TaskGroupId: null, Name: 'Test Task (BLOCKED)', Complete: true },
+        { Id: 2, TaskGroupId: 1, Name: 'Test Task (DELEGATED)', Complete: false },
+        { Id: 3, TaskGroupId: 1, Name: 'Test Task', Complete: false }
     ];
 
     let _filteredTasks = [];
@@ -36,8 +90,20 @@ import { completion, date, status } from '@/utils/filter-values';
 
     //// WHEN ////
 
-    function whenFiltersApplied() {
+    function whenAllFiltersApplied() {
         _filteredTasks = _filterer.filter();
+    }
+
+    function whenCompletionFilterApplied() {
+        _filteredTasks = _tasks.filter(task => _filterer.filterByCompletion(task));
+    }
+
+    function whenStatusFilterApplied() {
+        _filteredTasks = _tasks.filter(task => _filterer.filterByStatus(task));
+    }
+
+    function whenTaskGroupFilterApplied() {
+        _filteredTasks = _tasks.filter(task => _filterer.filterByTaskGroup(task));
     }
 
     //// THEN ////
--- src/utils/filterer.js
+++ src/utils/filterer.js
@@ -1,3 +1,5 @@
+import { completion, date, status } from '@/utils/filter-values';
+
 const Filterer = class Filterer {
     constructor(tasks, filters) {
         this.tasks = tasks;
@@ -5,14 +7,49 @@ const Filterer = class Filterer {
     }
 
     filter() {
-        return this.tasks.filter(this.taskGroupFilter);
+        return this.tasks.filter((task) => {
+            return (
+                this.filterByCompletion(task) &&
+                this.filterByStatus(task) &&
+                this.filterByTaskGroup(task)
+            );
+        });
     }
 
-    get taskGroupFilter() {
+    filterByCompletion(task) {
+        let result = true;
+
+        if (this.filters.completion == completion.COMPLETE) {
+            result = task.Complete;
+        }
+
+        else if (this.filters.completion == completion.INCOMPLETE) {
+            result = !task.Complete;
+        }
+
+        return result;
+    }
+
+    filterByStatus(task) {
+        let result = null;
+        this.filters.status = this.filters.status || [];
+
+        if (this.filters.status.includes(status.BLOCKED)) {
+            result = task.Name.toLowerCase().includes('blocked');
+        }
+
+        if (this.filters.status.includes(status.DELEGATED)) {
+            result = result || task.Name.toLowerCase().includes('delegated');
+        }
+
+        return result == null ? true : result;
+    }
+
+    filterByTaskGroup(task) {
         return (
             this.filters.taskGroupId
-                ? task => task.TaskGroupId == this.filters.taskGroupId
-                : () => true
+                ? task.TaskGroupId == this.filters.taskGroupId
+                : true
         );
     }
 };
