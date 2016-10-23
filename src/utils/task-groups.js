109e2f18591a7e8dcde06a18d62c379611f9ee62
Jacob Sowles <jacob.sowles@speakcreative.com>
2016-10-23 14:50:32 -0700

    feat: add task group helper class with tests

---
+++ src/utils/task-groups.js
@@ -0,0 +1,24 @@
+class TaskGroupHelpers {
+    constructor(taskGroups) {
+        this.taskGroups = taskGroups;
+    }
+
+    allDescendents = (taskGroup) => {
+        taskGroup =
+            typeof taskGroup == 'number'
+                ? this.taskGroups.find(tg => tg.Id == taskGroup)
+                : taskGroup;
+
+        let taskGroups = [ taskGroup ];
+
+        this.taskGroups
+            .filter(tg => tg.ParentTaskGroupId == taskGroup.Id)
+            .forEach((tg) => {
+                taskGroups = taskGroups.concat(this.allDescendents(tg));
+            });
+
+        return taskGroups;
+    };
+}
+
+export default TaskGroupHelpers;
