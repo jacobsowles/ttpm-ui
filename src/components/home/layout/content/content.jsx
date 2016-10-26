5307637f01dd6aab75f4ba30f82be8805796c4a1
Kate Sowles <katesowles@gmail.com>
2016-10-25 17:08:23 -0700

    feat: last few interface improvements for this round

--- src/components/home/layout/content/content.jsx
+++ src/components/home/layout/content/content.jsx
@@ -10,7 +10,7 @@ require('./content.scss');
 class Content extends React.Component {
     render() {
         return (
-            <div id="content" className="col-md-9 col-md-offset-3">
+            <div id="content" className="col-md-10 col-md-offset-2">
                 <ViewContainer defaultShowAnalytics={this.props.defaultShowAnalytics} />
             </div>
         );
