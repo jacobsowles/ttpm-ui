f4aea44e93e823be0f493a5f6045ae9c3b0f84c8
Jacob Sowles <jacob.sowles@speakcreative.com>
2017-03-10 12:33:06 -0800

    chore: create simple web server

---
+++ server.js
@@ -0,0 +1,7 @@
+const connect = require('connect');
+const serveStatic = require('serve-static');
+const port = process.env.PORT || 8080;
+
+connect().use(serveStatic(__dirname)).listen(port, function() {
+    console.log(`Server running on ${port}...`);
+});
